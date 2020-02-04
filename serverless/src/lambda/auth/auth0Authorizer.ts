fa28680667cbf8fdee2f619003cf5265f1f914b5
kafuuma <arkafuuma@gmail.com>
2020-02-04 17:36:56 -0500

    initial commit

---
+++ serverless/src/lambda/auth/auth0Authorizer.ts
@@ -0,0 +1,86 @@
+import { CustomAuthorizerEvent, CustomAuthorizerResult, CustomAuthorizerHandler } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+// import * as middy from 'middy'
+// import { secretsManager } from 'middy/middlewares'
+
+import { verify } from 'jsonwebtoken'
+import { JwtToken } from '../../auth/JwtToken'
+// import { Client } from 'elasticsearch'
+
+const client = new AWS.SecretsManager()
+
+//Cache secret if a Lambda instance is reused
+
+let cachedSecret : string
+
+const secretId = process.env.AUTH_0_SECRET_ID
+const secretField = process.env.AUTH_0_SECRET_FIELD
+
+export const handler:CustomAuthorizerHandler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
+  try {
+    const decodedToken = await verifyToken(
+      event.authorizationToken
+    )
+    console.log('User was authorized', decodedToken)
+
+    return {
+      principalId: decodedToken.sub,
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  } catch (e) {
+    console.log('User was not authorized', e.message)
+
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Deny',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  }
+}
+
+async function verifyToken(authHeader: string): Promise<JwtToken> {
+  if (!authHeader)
+    throw new Error('No authentication header')
+
+  if (!authHeader.toLowerCase().startsWith('bearer '))
+    throw new Error('Invalid authentication header')
+
+  const split = authHeader.split(' ')
+  const token = split[1]
+
+
+  const secretObject:any = await getSecret()
+  const secret = secretObject[secretField]
+
+  return verify(token, secret) as JwtToken
+}
+
+
+async function getSecret(){
+  if(cachedSecret) return cachedSecret
+
+  const data = await client.getSecretValue({
+    SecretId:secretId,
+  }).promise()
+  cachedSecret = data.SecretString
+
+  return JSON.parse(cachedSecret)
+}
